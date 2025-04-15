5ed6ace0d505a3ce3dc1ea3b62cc5030ba1f26da
Abraham Lillo <abraham.lillol@gmail.com>
2025-04-15 11:02:19 -0400

    devise listo , falta ver language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,20 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = session[:locale]||
+    extract_locale_from_accept_language_header ||
+    I18n.default_locale
+  end
+  def extract_locale_from_accept_language_header
+    request.env["HTTP_ACCEPT_LANGUAGE"].scan(/^[a-z]{2}/).first
+    locale if [ "es", "en" ].include? locale.to_s
+  end
+  def set_language
+    session[:locale] = params[:locale]
+    redirect_back(fallback_location: root_path)
+  end
 end
