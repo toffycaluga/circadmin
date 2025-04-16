0fcbd836d4fed8f784087077b3e40a32a4746855
Abraham Lillo <abraham.lillol@gmail.com>
2025-04-16 14:56:59 -0400

    boton de cierre de sesion listo , y pagna 404 ingresada

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,4 +24,7 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
   end
+  def render_not_found
+    redirect_to "/404"
+  end
 end
