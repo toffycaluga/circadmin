6887722978d2932be2ac80537a9ba308342fd02d
Abraham Lillo <abraham.lillol@gmail.com>
2025-04-17 10:38:29 -0400

    utilizando anotate y active storage en proyecto

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: user_profiles
+#
+#  id              :integer          not null, primary key
+#  user_id         :integer          not null
+#  nombre_completo :string
+#  direccion       :string
+#  pais            :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_user_profiles_on_user_id  (user_id)
+#
+
 class UserProfile < ApplicationRecord
   belongs_to :user
 end
