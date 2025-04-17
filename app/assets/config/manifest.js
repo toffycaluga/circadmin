39e6c5c49c9a06d72c226995587dc2202423b809
Abraham Lillo <abraham.lillol@gmail.com>
2025-04-17 14:25:02 -0400

    fix: estoy en proceso de reparar el tema de los layouts

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,9 +1,8 @@
 //= link_tree ../images
 
-// link_directory ../stylesheets/plugin .css
-//link_directory ../stylesheets/plugin .scss
-// link_directory ../stylesheets .scss
+
 //= link_directory ../stylesheets .css
+//= link_directory ../stylesheets .scss
 //= link application.scss
 //= link application.js
 
