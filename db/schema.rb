3f7185dbf497721a5798b7c78ca4e77aa7f3ddb6
Abraham Lillo <abraham.lillol@gmail.com>
2025-06-21 23:22:05 -0400

    migration de confirmable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_21_162128) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_21_180846) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -193,6 +193,11 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_21_162128) do
     t.integer "invitations_count", default: 0
     t.integer "inviting_circus_id"
     t.boolean "superadmin", default: false, null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
     t.index ["invited_by_id"], name: "index_users_on_invited_by_id"
